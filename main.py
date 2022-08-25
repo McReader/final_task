0359492c104ba68f3b8d1eb6b872f69eaae32402
Dzianis Roi <mcreader215@gmail.com>
2022-08-25 19:56:08 +0100

    feat: added --version support

--- main.py
+++ main.py
@@ -7,12 +7,14 @@ parser.add_argument('source', type=str, nargs=1,
                     help='RSS URL')
 
 parser.add_argument('--version', help='Print version info',
-                    action='store_false')
+                    action='version', version='%(prog)s 1.0')
 parser.add_argument(
     '--json', help='Print result as JSON in stdout', action='store_false')
 parser.add_argument(
     '--verbose', help='Outputs verbose status messages', action='store_false')
 parser.add_argument(
     '--limit', help='Limit news topics if this parameter provided')
+
+args = parser.parse_args()
 
 parser.print_help()
