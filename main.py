69d9af1ab9162471d24f266409a5c00386e06f32
Dzianis Roi <mcreader215@gmail.com>
2022-08-25 19:35:59 +0100

    chore: initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+import argparse
+
+parser = argparse.ArgumentParser(
+    description='Pure Python command-line RSS reader.')
+
+parser.add_argument('source', type=str, nargs=1,
+                    help='RSS URL')
+
+parser.add_argument('--version', help='Print version info',
+                    action='store_false')
+parser.add_argument(
+    '--json', help='Print result as JSON in stdout', action='store_false')
+parser.add_argument(
+    '--verbose', help='Outputs verbose status messages', action='store_false')
+parser.add_argument(
+    '--limit', help='Limit news topics if this parameter provided')
+
+parser.print_help()
