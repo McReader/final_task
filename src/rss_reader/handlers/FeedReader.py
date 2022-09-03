14871fccfd6efb1a0b9f3f22a57034d8872fc584
Dzianis Roi <mcreader215@gmail.com>
2022-09-03 14:08:55 +0100

    feat: updated FeadReader
    Updated feed reader to parse response and create a domain model from it

--- src/rss_reader/handlers/FeedReader.py
+++ src/rss_reader/handlers/FeedReader.py
@@ -2,6 +2,7 @@ import feedparser
 
 from .Handler import Handler
 from ..ExecutionContext import ExecutionContext
+from ..domain import Feed, FeedEntry
 
 
 class FeedReader(Handler):
@@ -10,21 +11,58 @@ class FeedReader(Handler):
     def __init__(self):
         super().__init__(FeedReader.__name__)
 
-    def handle(self, ctx: ExecutionContext) -> None:
-        """Handles the feed download operation from the "source" specified by user.
+    def handle(self, ctx: ExecutionContext):
+        """Loads the feed from the "source" specified by user.
 
         Keyword arguments:
             ctx: execution context of the entire chain
         """
+        feed = self.load(ctx.args.source)
+
+        ctx.feed = feed
+
+    def load(self, source: str) -> Feed:
+        """Loads the feed from the "source" specified by user.
+
+        Keyword arguments:
+            source: feed source
+
+        Returns:
+            feed
+        """
         try:
-            response = feedparser.parse(ctx.args.source)
+            response = feedparser.parse(source)
             if response.bozo:
                 raise response.bozo_exception
         except (feedparser.CharacterEncodingUnknown, feedparser.CharacterEncodingOverride) as e:
             raise Exception("The feed has incorrectly-declared encodings")
         except feedparser.NonXMLContentType as e:
             raise Exception("The feed is not XML")
-        except Exception as e:
-            raise Exception(f"Failed to read feed due to unknown error: {e}")
+        except Exception:
+            raise Exception(f"Failed to read feed due to unknown error")
 
-        ctx.feed = response.feed
+        feed = self._create_feed(response)
+
+        return feed
+
+    def _create_feed(self, response: dict) -> Feed:
+        """Creates the feed from the response"""
+        return Feed(
+            title=response.title,
+            link=response.link,
+            description=response.description,
+            entries=self._create_entries(response.entries)
+        )
+
+    def _create_entries(self, entries: list) -> list:
+        """Creates the feed entries from the response"""
+        return [self._create_entry(entry) for entry in entries]
+
+    def _create_entry(self, entry: dict) -> FeedEntry:
+        """Creates the feed entry from the response"""
+        return FeedEntry(
+            title=entry.title,
+            link=entry.link,
+            description=entry.description,
+            published=entry.published
+        )
