c960d445c627b574d10b1965674a53cfdb82fa01
Dzianis Roi <mcreader215@gmail.com>
2022-09-09 23:28:03 +0100

    feat: updated html formatter

--- src/rss_reader_cli/formatter/HtmlFormatter.py
+++ src/rss_reader_cli/formatter/HtmlFormatter.py
@@ -7,15 +7,36 @@ class HtmlFormatter(Formatter):
     def format(self, entries: list[dict]) -> str:
         """Format the feed entries as HTML"""
 
-        html = f"""<html>
-        <head>
-            <title>{data['title']}</title>
-        </head>
-        <body>
-            <h1>{data['title']}</h1>
-            <p>{data['description']}</p>
-            <p>{data['link']}</p>
-            <p>{data['pubDate']}</p>
-        </body>
-        </html>"""
+        html = f"""
+<html>
+    <head>
+        <title>News Feed</title>
+    </head>
+    <body>
+        <ul>
+            {self._render_items(entries)}
+        </ul>
+    </body>
+</html>
+        """
         return html
+
+    def _render_items(self, entries: list[dict]) -> str:
+        """Render the feed entries as HTML"""
+
+        return '\n'.join([self._render_item(entry) for entry in entries])
+
+    def _render_item(self, entry: dict) -> str:
+        """Render the feed entry as HTML"""
+
+        return f"""
+            <li>
+                <a href={entry.get('link')}>
+                    <h2>
+                        {entry.get('title')}
+                    </h2>
+                </a>
+                <p>{entry.get('description')}</p>
+                <p>{entry.get('published')}</p>
+            </li>
+        """
