6f1be74477f8bd63891b28ae3d72f3b46b888f69
Dzianis Roi <mcreader215@gmail.com>
2022-09-12 00:29:14 +0100

    chore: added coverage report

--- README.md
+++ README.md
@@ -9,3 +9,15 @@ To run the tests you need to execute the following command
 ```bash
 python -m unittest discover -s src -p '*_test.py'
 ```
+
+Coverage is analysed using [coverage](https://pypi.org/project/coverage/) package. To run tests with coverage report, use
+
+```bash
+python -m coverage run -m unittest discover -s src -p '*_test.py'
+```
+
+To print the report to the terminal, use
+
+```bash
+python -m coverage report
+```
--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup, find_packages
+from setuptools import setup, find_packages, find_namespace_packages
 
 setup(
     name='rss_reader',
@@ -9,7 +9,17 @@ setup(
     author_email='mcreader215@gmail.com',
     license='MIT',
     packages=find_packages(where="src"),
-    install_requires=['argparse'],
+    install_requires=[
+        'argparse==1.4.0',
+        'coverage==6.4.4',
+        'dataclasses==0.8',
+        'datetime==4.5',
+        'enum==0.4.7',
+        'FB2==0.1.7',
+        'logging==0.4.9.6',
+        'requests==6.0.10',
+        'tinydb==4.7.0'
+    ],
     python_requires='>=3.9',
     package_dir={"": "src"},
     entry_points={
